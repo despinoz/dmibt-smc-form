ceba3d557cca6d6f87662f758591879f21c1e31d
Diego Espinoza <diespinozas@gmail.com>
2019-02-14 10:07:49 -0500

    add basic header to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  header: {
+    background: '#07436F',
+    color: 'white',
+    height: '60px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <h1>DMIBT SMC Form</h1>
+        <Typography variant="h6" className={classes.header}>
+          DMIBT SMC Form
+        </Typography>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
