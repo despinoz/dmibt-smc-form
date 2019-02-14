aa24b90876e301f074a410a36f9581006e974543
Diego Espinoza <diespinozas@gmail.com>
2019-02-14 16:23:33 -0500

    add mobile and browser version

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Select from '@material-ui/core/Select';
 import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import { BrowserView, MobileView } from 'react-device-detect';
 import {
   areaSolicitanteOpc,
   tipoOrdenOpc,
@@ -143,174 +144,179 @@ class App extends Component {
     const { sst, suministro, cantidadActividades } = this.state;
     return (
       <div>
-        <Typography variant="h6" className={classes.header}>
-          DMIBT SMC Form
-        </Typography>
-        <div className={classes.twoFields}>
-          <TextField
-            id="sst"
-            label="SST"
-            type="number"
-            value={sst}
-            className={classes.fifty}
-            onChange={this.handleFixedChange}
-          />
-          <TextField
-            id="suministro"
-            label="Suministro"
-            type="number"
-            value={suministro}
-            className={classes.fifty}
-            onChange={this.handleFixedChange}
-          />
-        </div>
-        <form className={classes.container}>
-          <FormControl>
-            <InputLabel>Area Solicitante</InputLabel>
-            <Select native id="areaSolicitante" onChange={this.handleChange}>
-              {areaSolicitanteOpc.map(opc => (
-                <option key={opc.value} value={opc.value}>
-                  {opc.label}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl>
-            <InputLabel>Tipo Orden</InputLabel>
-            <Select native id="tipoOrden" onChange={this.handleChange}>
-              {tipoOrdenOpc.map(opc => (
-                <option key={opc.value} value={opc.value}>
-                  {opc.label}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
+        <BrowserView>
+          <Typography variant="h3">En Construccion</Typography>
+        </BrowserView>
+        <MobileView>
+          <Typography variant="h6" className={classes.header}>
+            DMIBT SMC Form
+          </Typography>
           <div className={classes.twoFields}>
+            <TextField
+              id="sst"
+              label="SST"
+              type="number"
+              value={sst}
+              className={classes.fifty}
+              onChange={this.handleFixedChange}
+            />
+            <TextField
+              id="suministro"
+              label="Suministro"
+              type="number"
+              value={suministro}
+              className={classes.fifty}
+              onChange={this.handleFixedChange}
+            />
+          </div>
+          <form className={classes.container}>
             <FormControl>
-              <InputLabel>Actividad 1</InputLabel>
-              <Select
-                native
-                id="actividad1"
-                className={classes.actividad1}
-                onChange={this.handleChange}
+              <InputLabel>Area Solicitante</InputLabel>
+              <Select native id="areaSolicitante" onChange={this.handleChange}>
+                {areaSolicitanteOpc.map(opc => (
+                  <option key={opc.value} value={opc.value}>
+                    {opc.label}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl>
+              <InputLabel>Tipo Orden</InputLabel>
+              <Select native id="tipoOrden" onChange={this.handleChange}>
+                {tipoOrdenOpc.map(opc => (
+                  <option key={opc.value} value={opc.value}>
+                    {opc.label}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+            <div className={classes.twoFields}>
+              <FormControl>
+                <InputLabel>Actividad 1</InputLabel>
+                <Select
+                  native
+                  id="actividad1"
+                  className={classes.actividad1}
+                  onChange={this.handleChange}
+                >
+                  {actividadesOpc.map(opc => (
+                    <option key={opc.value} value={opc.value}>
+                      {opc.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <Icon
+                className={classes.icon}
+                onClick={this.addActividad}
+                color="primary"
               >
-                {actividadesOpc.map(opc => (
-                  <option key={opc.value} value={opc.value}>
-                    {opc.label}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-            <Icon
-              className={classes.icon}
-              onClick={this.addActividad}
-              color="primary"
+                add_circle
+              </Icon>
+            </div>
+            {cantidadActividades >= 2 && (
+              <FormControl>
+                <InputLabel>Actividad 2</InputLabel>
+                <Select native id="actividad2" onChange={this.handleChange}>
+                  {actividadesOpc.map(opc => (
+                    <option key={opc.value} value={opc.value}>
+                      {opc.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+            {cantidadActividades >= 3 && (
+              <FormControl>
+                <InputLabel>Actividad 3</InputLabel>
+                <Select native id="actividad3" onChange={this.handleChange}>
+                  {actividadesOpc.map(opc => (
+                    <option key={opc.value} value={opc.value}>
+                      {opc.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+            {cantidadActividades >= 4 && (
+              <FormControl>
+                <InputLabel>Actividad 4</InputLabel>
+                <Select native id="actividad4" onChange={this.handleChange}>
+                  {actividadesOpc.map(opc => (
+                    <option key={opc.value} value={opc.value}>
+                      {opc.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+            <TextField
+              id="observacion"
+              label="Observación"
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="contacto"
+              label="Contacto"
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="telefonoRef"
+              label="Teléfono Ref"
+              type="number"
+              onChange={this.handleChange}
+            />
+            <div className={classes.twoFields}>
+              <FormControl className={classes.fifty}>
+                <InputLabel>Tipo Atención</InputLabel>
+                <Select native id="tipoAtencion" onChange={this.handleChange}>
+                  {solicitudAtencionOpc.map(opc => (
+                    <option key={opc.value} value={opc.value}>
+                      {opc.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <TextField
+                id="codigoAtencion"
+                label="Código Atención"
+                className={classes.fifty}
+                onChange={this.handleChange}
+              />
+            </div>
+            <TextField
+              id="fechaProg"
+              label="Fecha Prog"
+              type="date"
+              onChange={this.handleChange}
+            />
+            <div className={classes.twoFields}>
+              <TextField
+                id="horaProg"
+                type="time"
+                label="Hora Prog"
+                className={classes.fifty}
+                onChange={this.handleChange}
+              />
+              <TextField
+                id="horaFinProg"
+                type="time"
+                label="Hora Fin Prog"
+                className={classes.fifty}
+                onChange={this.handleChange}
+              />
+            </div>
+            <Button
+              variant="contained"
+              size="medium"
+              className={classes.register}
+              onClick={this.saveToFirebase}
             >
-              add_circle
-            </Icon>
-          </div>
-          {cantidadActividades >= 2 && (
-            <FormControl>
-              <InputLabel>Actividad 2</InputLabel>
-              <Select native id="actividad2" onChange={this.handleChange}>
-                {actividadesOpc.map(opc => (
-                  <option key={opc.value} value={opc.value}>
-                    {opc.label}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-          {cantidadActividades >= 3 && (
-            <FormControl>
-              <InputLabel>Actividad 3</InputLabel>
-              <Select native id="actividad3" onChange={this.handleChange}>
-                {actividadesOpc.map(opc => (
-                  <option key={opc.value} value={opc.value}>
-                    {opc.label}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-          {cantidadActividades >= 4 && (
-            <FormControl>
-              <InputLabel>Actividad 4</InputLabel>
-              <Select native id="actividad4" onChange={this.handleChange}>
-                {actividadesOpc.map(opc => (
-                  <option key={opc.value} value={opc.value}>
-                    {opc.label}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-          <TextField
-            id="observacion"
-            label="Observación"
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="contacto"
-            label="Contacto"
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="telefonoRef"
-            label="Teléfono Ref"
-            type="number"
-            onChange={this.handleChange}
-          />
-          <div className={classes.twoFields}>
-            <FormControl className={classes.fifty}>
-              <InputLabel>Tipo Atención</InputLabel>
-              <Select native id="tipoAtencion" onChange={this.handleChange}>
-                {solicitudAtencionOpc.map(opc => (
-                  <option key={opc.value} value={opc.value}>
-                    {opc.label}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-            <TextField
-              id="codigoAtencion"
-              label="Código Atención"
-              className={classes.fifty}
-              onChange={this.handleChange}
-            />
-          </div>
-          <TextField
-            id="fechaProg"
-            label="Fecha Prog"
-            type="date"
-            onChange={this.handleChange}
-          />
-          <div className={classes.twoFields}>
-            <TextField
-              id="horaProg"
-              type="time"
-              label="Hora Prog"
-              className={classes.fifty}
-              onChange={this.handleChange}
-            />
-            <TextField
-              id="horaFinProg"
-              type="time"
-              label="Hora Fin Prog"
-              className={classes.fifty}
-              onChange={this.handleChange}
-            />
-          </div>
-          <Button
-            variant="contained"
-            size="medium"
-            className={classes.register}
-            onClick={this.saveToFirebase}
-          >
-            <SaveIcon />
-            Registrar
-          </Button>
-        </form>
+              <SaveIcon />
+              Registrar
+            </Button>
+          </form>
+        </MobileView>
       </div>
     );
   }
